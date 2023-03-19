1e15ba40575dff11a15cef660fa4dab184dac7c0
Pedro Henrique <pedrohenri1606@gmail.com>
2023-03-19 14:29:54 -0300

    First Commit

---
+++ src/main/java/com/projetoSpring/projetoCurso/ProjetoCursoApplication.java
@@ -0,0 +1,13 @@
+package com.projetoSpring.projetoCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoSpring/projetoCurso/ProjetoCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoSpring.projetoCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
